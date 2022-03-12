fce5ed366a315f63206b678cce37a3468b348cd2
Whoami <jdgomezdelgado@uts.edu.co>
2022-03-12 00:19:57 -0500

    funcionamiento diferente

--- consumo_normal.php
+++ consumo_normal.php
@@ -2,16 +2,7 @@
 ini_set('max_execution_time', 0);
 set_time_limit(0);
 $url = "http://desercion.uts.edu.co/docentes/funciones/buscar_id_academusoft.php";
-$posibles_correos = array('juanjosesilva@uts.edu.co', 'juanjsilva@uts.edu.co');
-for ($i=1002100000; $i < 1003108571 ; $i++) { 
-	if (in_array(buscarAmigo($i, $url), $posibles_correos) ) {
-		echo $i;
-		echo "I finally found u";
-	}
-	
-}
-function buscarAmigo($cedula, $url)
-{
+$cedula = $_GET['cedula'];
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -19,11 +10,10 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, "tipo=buscar_cedula&bus={$cedula}");
 $result = curl_exec($ch);
-$json = json_decode($result, true);
-return $json['correo'];
+echo json_encode(json_decode($result, true));
 if (curl_errno($ch)) {
     echo 'Error:' . curl_error($ch);
 }
 curl_close($ch);
-}
+
 ?>
